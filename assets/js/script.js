f4ba2260545f37c9676d6297fdfbf72b19efdafc
Amir Rastkhadiv <amir.rastkhadiv@gmail.com>
2022-09-08 10:49:22 +0000

    Set up structure of game functions.

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incremnetScore() {
+
+}
+
+function incrementWrongAnswers() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function dispalySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function dispalyDivisionQuestion() {
+
+}
